283f2df75ac9a28d1b628b5bec9f555592ca99a7
erik148 <erik.trabalho90@gmail.com>
2025-01-11 16:15:02 -0300

    starbucks-commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+let circulo = document.querySelector('.circulo')
+
+let imagem = document.querySelector('.copo')
+
+function TrocarACor (cor){
+ circulo.style.background = cor
+
+
+}
+
+function TrocarImagem(endereco){
+    imagem.src = endereco 
+    
+    
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
